c0530eda24078036ae83b40df2a6bb8da7f10807
Rafal Rokoszewski <rafbr1@gmail.com>
2016-12-29 15:45:58 +0100

    final, course finished

---
+++ APM/app/app.ts
@@ -0,0 +1,22 @@
+module app {
+	const main = angular.module("productManagement", ["ngRoute", "common.services"]);
+
+	main.config(routeConfig);
+
+	routeConfig.$inject = ["$routeProvider"];
+	function routeConfig($routeProvider: ng.route.IRouteProvider): void {
+		
+		$routeProvider
+			.when("/productList",
+			{
+				templateUrl: "/app/products/productListView.html",
+				controller: "ProductListCtrl as vm"
+			})
+			.when("/productDetail/:productId",
+			{
+				templateUrl: "/app/products/productDetailView.html",
+				controller: "ProductDetailCtrl as vm"
+			})
+			.otherwise("/productList");
+	}
+}
